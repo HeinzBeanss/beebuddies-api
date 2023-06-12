d08dc214250913d1b1a6a3d91221d2e1ed1c14b4
David Bean <heinzbeansss@gmail.com>
2023-06-12 03:54:00 +0100

    Fix incorrect URLs

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,7 +52,7 @@ exports.login_facebook_callback = [
           res.cookie('token', token);
 
           // Redirect to the login page
-          res.redirect("https://heinzbeanss.github.io/beebuddies/#/login");
+          res.redirect("https://heinzbeanss.github.io/beebuddies");
     }
   ];
 
